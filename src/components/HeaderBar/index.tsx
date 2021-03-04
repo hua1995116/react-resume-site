8a55021a62a813669ae6f1ec895871f0752b5eb2
huayifeng <461249104@qq.com>
2021-03-05 00:21:32 +0800

    feat: feedback

--- src/components/HeaderBar/index.tsx
+++ src/components/HeaderBar/index.tsx
@@ -2,17 +2,18 @@ import React, { useState, useEffect } from 'react';
 import { Menu, Dropdown } from 'antd';
 import "./index.less";
 import { getTheme } from '@utils/changeThemes';
+import { markdownParserResume } from '@utils/helper';
 
 const HeaderBar = () => {
   const [template, setTemplate] = useState('default');
   const menu = (
     <Menu>
       <Menu.Item>
-        <a rel="noopener noreferrer" onClick={(e) => {
+        <a rel="noopener noreferrer" onClick={async (e) => {
           e.preventDefault()
           if (template !== 'default') {
             setTemplate('default');
-            getTheme('default');
+            await getTheme('default');
             document.body.style.setProperty('--bg', '#39393a');
           }
         }}>
@@ -20,11 +21,11 @@ const HeaderBar = () => {
         </a>
       </Menu.Item>
       <Menu.Item>
-        <a rel="noopener noreferrer" onClick={(e) => {
+        <a rel="noopener noreferrer" onClick={async (e) => {
           e.preventDefault()
           if (template !== 'blue') {
             setTemplate('blue');
-            getTheme('blue');
+            await getTheme('blue');
             document.body.style.setProperty('--bg', '#5974D4');
           }
         }}>
@@ -33,24 +34,64 @@ const HeaderBar = () => {
       </Menu.Item>
     </Menu>
   );
+
+  const feedbackMenu = (
+    <Menu>
+      <Menu.Item>
+        <div className="rs-feed-group">
+          <div className="rs-feed-group__wechat">
+            <div className="rs-feed-group__text">微信群</div>
+            <div className="bg"></div>
+          </div>
+          <div className="rs-feed-group__qq">
+            <div className="rs-feed-group__text">qq群(699817990)</div>
+            <div className="bg"></div>
+          </div>
+        </div>
+      </Menu.Item>
+    </Menu>
+  )
+
+  const exportPdf = () => {
+    const content = localStorage.getItem('md-resume');
+    if (content) {
+      const body = markdownParserResume.render(content);
+      const styles = document.getElementById('rs-themes-data');
+      const backgroundColor = getComputedStyle(document.body).getPropertyValue("--bg");
+    }
+  }
+
   useEffect(() => {
     getTheme('default');
   }, []);
   return (
     <div className="rs-header-bar rs-link">
-      <a className="rs-logo rs-link">
-        ✍️ 木及简历
-      </a>
-      <a href="#" className="rs-link">
-        文件
-      </a>
-      {/* <div className="rs-select-template"> */}
+      <div className="rs-header-bar__left">
+        <a className="rs-logo rs-link">
+          ✍️ 木及简历
+        </a>
+        <a href="#" className="rs-link">
+          文件
+        </a>
         <Dropdown overlay={menu} trigger={['click']}> 
           <a className="ant-dropdown-link rs-link" onClick={e => e.preventDefault()}>
             选择模板
           </a>
         </Dropdown>
-      {/* </div> */}
+        <a href="#" className="rs-link" onClick={ exportPdf }>
+          导出 pdf
+        </a>
+      </div>
+      <div className="rs-header-bar__right">
+        {/* <a className="rs-link">
+          交流与反馈
+        </a> */}
+        <Dropdown overlay={feedbackMenu}> 
+          <a className="ant-dropdown-link rs-link" onClick={e => e.preventDefault()}>
+            交流与反馈
+          </a>
+        </Dropdown>
+      </div>
     </div>
   )
 }

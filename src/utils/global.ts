3384c7d22b4de2186e6394b6f4469e46656f97cb
huayifeng <461249104@qq.com>
2021-03-11 01:32:12 +0800

    chore: update default template

--- src/utils/global.ts
+++ src/utils/global.ts
@@ -10,20 +10,20 @@ export const INIT_CONTENT = `
 
 浙江科技学院/信息与计算科学
 
-本科/2018年毕业/党员
+本科/2018年毕业/党员 
 
 :::
 
 ::: right
-https://qiufeng.blue
+[icon:blog https://qiufeng.blue](https://qiufeng.blue)
 
-https://github.com/hua1995116
+[icon:github https://github.com/hua1995116]()
 
-[掘金](https://juejin.cn/user/923245497557111) 
+[icon:juejin 掘金](https://juejin.cn/user/923245497557111) 
 
-qiufenghyf@163.com
+[icon:email qiufenghyf@163.com](mailto:qiufenghyf@163.com)
 
-178****8380
+icon:phone 178****8380
 
 :::
 
@@ -38,8 +38,10 @@ qiufenghyf@163.com
 ## 工作
 
 ### 杭州兑吧网络有限公司 - 实习 （2018.3-2018.7）
+`工程化` `基建搭建`
 
 前端架构组
+
 - 前端错误监控系统(基建)(负责人) 接入量pv:3000w
   - web端js-sdk开发,无侵入式接入,压缩后仅2kb。
   - 收集端Node开发,分布式存储日志。
@@ -57,10 +59,34 @@ qiufenghyf@163.com
 - webpack插件(webpack-plugin-inner-script)地址
   - 自动将外链形式改写成内敛形式。
 
+### 网易 - 实习 (2017.11-2018.3)
+信息技术部
+
+`业务开发` `基建搭建`
+
+- 前端监控系统
+  - 客户端监控载体端以及数据脚本开发
+  - 基于highcharts的可视化图形界面开发
+  - 平台的性能统计模块以及告警模块开发
+  - 高并发下的redis设计以及错误信息识别过滤处理
+- 内网准入系统
+  - 采用TypeScript+ES6/7+React开发
+  - 基本多层级树形结构的黑名单/白名单控制组件
+
+### 51信用卡 - 实习(2017.6-2017.11)
+金融产品组
+
+- 开发维护产品51零用钱,h5端以及微信端,日活跃用户为20w+
+- 微信端活动--黄金金挖矿工,吸粉2.5w+,活动期间日活跃用户1000+。
+- 性能优化以及体验优化的开发。首屏优化，70%的用户1.5s内进入。
+- 基于Vue的下拉刷新和图片懒加载组件开发
+
 ## 项目
 
 ### 实时聊天项目(webchat)
-Github地址：[https://github.com/hua1995116/webchat](https://github.com/hua1995116/webchat)（聊天室移动端，注册用户超过7000+，star将近1k）
+`Vue2.0` `Websocket` `Node.JS` `MongoDB`
+
+在线地址：[https://www.qiufengh.com/](https://www.qiufengh.com/)（聊天室移动端，注册用户超过7000+，star将近1k）
   
 负责前端构建,服务器架构,后端开发
   
@@ -70,6 +96,8 @@ Github地址：[https://github.com/hua1995116/webchat](https://github.com/hua199
 - 开发Loading，Alert，Confirm等组件
 - 拥有日志系统以及消息的RSA算法加密
 - 基于redis的请求监控，过滤恶意请求
+
+
 
 ## 技能
 ### Web基础
